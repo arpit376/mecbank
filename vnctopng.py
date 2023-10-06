d5dace1223f36b90bd148f6656f2d95e75ee0fa5
arpit376 <arpittripathi@hotmail.com>
2023-10-06 09:51:04 +0530

    Update vnctopng.py

--- vnctopng.py
+++ vnctopng.py
@@ -11,7 +11,7 @@ for packet in decoder.packet_generator():
     if packet.has_screen():
         # Render the screen updates and save as an image
         image = packet.get_screen().make_image()
-        image.save(f'vnc_image_{packet.timestamp}.png')
+        image.save(f'vnc_image_.png')
 
 # Close the PCAP file
 decoder.close_pcap()
